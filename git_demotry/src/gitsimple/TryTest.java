53584efab0be6d6540c844bea1d9fcd85f619839
lohith <lohith725@gmail.com>
2018-11-09 23:13:17 +0530

    First commit in git

---
+++ git_demotry/src/gitsimple/TryTest.java
@@ -0,0 +1,21 @@
+package gitsimple;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TryTest {
+	
+	@Test
+	public void simp(){
+		
+		System.out.println("---------GIT---------");	
+		
+		 System.setProperty("webdriver.chrome.driver","G:\\Java_Software_Latest_version\\2.4 chrome driver\\chromedriver.exe");
+		 ChromeDriver driver=new ChromeDriver();
+		 driver.get("http://www.google.com");
+		 String s=driver.getTitle();
+		 System.out.println(s);
+		
+	}
+
+}
